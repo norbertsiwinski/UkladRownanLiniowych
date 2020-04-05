bd2d443d506fbf3f177c869cdc64de239ac92ba9
norbertsiwinski <62208115+norbertsiwinski@users.noreply.github.com>
2020-04-06 00:43:54 +0200

    tydzien 1

---
+++ inc/Macierz.hh
@@ -0,0 +1,75 @@
+#ifndef MACIERZ_HH
+#define MACIERZ_HH
+
+#include "rozmiar.h"
+#include <iostream>
+#include "Wektor.hh"
+
+
+/*
+ *  Tutaj trzeba opisac klase. Jakie pojecie modeluje ta klasa
+ *  i jakie ma glowne cechy.
+ */
+class Macierz {
+
+
+Wektor tab[ROZMIAR];
+
+  public:
+
+/* gettery i cettery */
+  Wektor & operator[](int index);
+const Wektor & operator[](int index)const;
+
+  
+Macierz();
+Macierz(Wektor A, Wektor B, Wektor C);
+
+const Wektor  operator *(const Wektor & B);
+const Macierz operator +(const Macierz & B);
+const Macierz operator -(const Macierz & B);
+const Macierz operator *(const Macierz & B);
+const Macierz operator *(double B);
+
+
+const Macierz transponuj() const;
+void Transponuj();
+
+const Macierz odwracaj() const;
+
+double wyznacznik(); /* na razie tylo SARRUS */
+
+
+
+};
+
+
+
+
+
+/*
+ * To przeciazenie trzeba opisac. Co ono robi. Jaki format
+ * danych akceptuje. Jakie jest znaczenie parametrow itd.
+ * Szczegoly dotyczace zalecen realizacji opisow mozna
+ * znalezc w pliku:
+ *    ~bk/edu/kpo/zalecenia.txt 
+ */
+std::istream& operator >> (std::istream &Strm, Macierz &Mac);
+std::ostream& operator << (std::ostream &Strm, const Macierz &Mac);
+/*
+ * To przeciazenie trzeba opisac. Co ono robi. Jaki format
+ * danych akceptuje. Jakie jest znaczenie parametrow itd.
+ * Szczegoly dotyczace zalecen realizacji opisow mozna
+ * znalezc w pliku:
+ *    ~bk/edu/kpo/zalecenia.txt 
+ */
+
+
+
+
+
+
+
+
+
+#endif
---
+++ inc/Wektor.hh
@@ -0,0 +1,56 @@
+#ifndef WEKTOR_HH
+#define WEKTOR_HH
+
+#include "rozmiar.h"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+/*
+ *  Tutaj trzeba opisac klase. Jakie pojecie modeluje ta klasa
+ *  i jakie ma glowne cechy.
+ */
+class Wektor {
+
+   double tab[ROZMIAR];
+
+  public:
+
+  Wektor(double xx, double yy, double zz); 
+  Wektor();
+
+double & operator [] (int index);
+  const double & operator [] (int index) const;
+  
+
+  const Wektor operator+(const Wektor & W2) const; 
+  const Wektor operator-(const Wektor & W2) const; 
+   double operator*(const Wektor & W2) const;
+  const Wektor operator*(double l2)const; 
+   
+ 
+  double dlugosc() const;
+
+  bool operator==(const Wektor & W2)const;
+  bool operator!=(const Wektor & W2)const;
+
+  
+
+};
+
+ Wektor operator*(double l1, const Wektor & W2); 
+
+std:: ostream & operator << (std::ostream &strm, const Wektor &W);
+std::istream& operator >> (std::istream &Strm, Wektor &Wek);
+
+/*
+ * To przeciazenie trzeba opisac. Co ono robi. Jaki format
+ * danych akceptuje. Jakie jest znaczenie parametrow itd.
+ * Szczegoly dotyczace zalecen realizacji opisow mozna
+ * znalezc w pliku:
+ *   ~bk/edu/kpo/zalecenia.txt 
+ */
+
+
+#endif
